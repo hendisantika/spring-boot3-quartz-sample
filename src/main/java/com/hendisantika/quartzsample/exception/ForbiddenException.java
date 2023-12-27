16ca6acca7404673c8888fea464781fa41d35777
hendisantika <hendisantika@yahoo.co.id>
2023-12-27 08:13:54 +0700

    Add ForbiddenException 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzsample/exception/ForbiddenException.java
@@ -0,0 +1,28 @@
+package com.hendisantika.quartzsample.exception;
+
+import org.hibernate.service.spi.ServiceException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-quartz-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/27/23
+ * Time: 08:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class ForbiddenException extends ServiceException {
+
+    public ForbiddenException() {
+        super();
+    }
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+    public ForbiddenException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}

55397e91c16cb3be2269d334962ff75146b29302
hendisantika <hendisantika@yahoo.co.id>
2023-12-27 08:16:49 +0700

    Add BaseControllerAdvice 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzsample/exception/base/BaseControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.hendisantika.quartzsample.exception.base;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-quartz-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/27/23
+ * Time: 08:16
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestControllerAdvice
+public class BaseControllerAdvice {
+
+    public static final Timestamp TIMESTAMP = new Timestamp(System.currentTimeMillis());
+
+}

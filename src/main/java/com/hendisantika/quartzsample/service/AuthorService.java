bff76f719512612db7b302458df89807e57f439b
hendisantika <hendisantika@yahoo.co.id>
2023-12-27 08:29:25 +0700

    Add AuthorService 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzsample/service/AuthorService.java
@@ -0,0 +1,22 @@
+package com.hendisantika.quartzsample.service;
+
+import com.hendisantika.quartzsample.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-quartz-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/27/23
+ * Time: 08:29
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+}

1efe8485b46989af1b94a90a3d1856e90b7dba7c
hendisantika <hendisantika@yahoo.co.id>
2023-12-27 08:24:59 +0700

    Add more method in PostRepository 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/quartzsample/repository/PostRepository.java
+++ src/main/java/com/hendisantika/quartzsample/repository/PostRepository.java
@@ -2,7 +2,12 @@ package com.hendisantika.quartzsample.repository;
 
 import com.hendisantika.quartzsample.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +21,13 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByTitleContaining(String title);
+
+    @Modifying
+    @Query(value = "SELECT * FROM posts WHERE title LIKE %:title%", nativeQuery = true)
+    List<Post> selectPosts(@Param("title") String title);
+
+    @Modifying
+    @Query("DELETE FROM Post p WHERE p.title = :title OR p.body = :body")
+    int deletePosts(@Param("title") String title, @Param("body") String body);
 }

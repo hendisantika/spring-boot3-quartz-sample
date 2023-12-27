5a254790caef1df0b4337a918ab4750222b91319
hendisantika <hendisantika@yahoo.co.id>
2023-12-27 08:12:45 +0700

    Add BadRequestException 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzsample/exception/BadRequestException.java
@@ -0,0 +1,32 @@
+package com.hendisantika.quartzsample.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-quartz-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/27/23
+ * Time: 08:12
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.hibernate.service.spi.ServiceException;
+
+/**
+ * trigger for bad request exception
+ */
+public class BadRequestException extends ServiceException {
+
+    public BadRequestException() {
+        super();
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}

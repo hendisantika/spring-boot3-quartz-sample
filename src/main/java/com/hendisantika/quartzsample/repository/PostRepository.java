5635fea6597a86e4bcd1071bd407da1a65e66eb0
hendisantika <hendisantika@yahoo.co.id>
2023-12-27 08:24:38 +0700

    Add PostRepository 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzsample/repository/PostRepository.java
@@ -0,0 +1,19 @@
+package com.hendisantika.quartzsample.repository;
+
+import com.hendisantika.quartzsample.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-quartz-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/27/23
+ * Time: 08:24
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}

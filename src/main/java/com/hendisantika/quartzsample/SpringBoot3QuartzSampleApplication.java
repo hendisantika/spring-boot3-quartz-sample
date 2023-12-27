0f6a4639258047d2268e7700a2f457bb7428a1ba
hendisantika <hendisantika@yahoo.co.id>
2023-12-27 07:49:01 +0700

    Create starter project 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzsample/SpringBoot3QuartzSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.quartzsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3QuartzSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3QuartzSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/quartzsample/SpringBoot3QuartzSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.quartzsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3QuartzSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
